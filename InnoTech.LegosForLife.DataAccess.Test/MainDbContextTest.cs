321438ed09742a95d7fc78737b2189841c6302fb
Kyle Collie <kcollie@hotmail.com>
2021-11-05 08:10:09 -0400

    Add MainDbContext Add Test Project for DataAccess

---
+++ InnoTech.LegosForLife.DataAccess.Test/MainDbContextTest.cs
@@ -0,0 +1,35 @@
+﻿using EntityFrameworkCore.Testing.Moq;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace InnoTech.LegosForLife.DataAccess.Test
+{
+    public class MainDbContextTest
+    {
+        [Fact]
+        public void DbContext_WithDbContextOptions_IsAvailable()
+        {
+            var mockedDbContext = Create.MockedDbContextFor<MainDbContext>();
+            Assert.NotNull(mockedDbContext);
+        }
+
+        [Fact]
+        public void DbContext_DbSets_MustHaveDbSetWithTypeProduct()
+        {
+
+        }
+    }
+
+    public class MainDbContext: DbContext
+    {
+        public MainDbContext(DbContextOptions<MainDbContext> options): base(options)
+        {
+
+        }
+    }
+}
