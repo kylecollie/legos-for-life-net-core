ebc89a9d0575540e2ddbd07e71f846c892fea3b3
Kyle Collie <kcollie@hotmail.com>
2021-11-06 07:43:26 -0400

    Add ProductRepo test

---
+++ InnoTech.LegosForLife.DataAccess.Test/ProductRepositoryTest.cs
@@ -0,0 +1,16 @@
+﻿using Innotech.LegosForLife.DataAccess.Repositories;
+using InnoTech.LegosForLife.Domain.IRepositories;
+using Xunit;
+
+namespace InnoTech.LegosForLife.DataAccess.Test
+{
+    public class ProductRepositoryTest
+    {
+        [Fact]
+        public void ProductRepository_IsIProductRepository()
+        {
+            var repository = new ProductRepository();
+            Assert.IsAssignableFrom<IProductRepository>(repository);
+        }
+    }
+}
---
+++ Innotech.LegosForLife.DataAccess/Repositories/ProductRepository.cs
@@ -0,0 +1,15 @@
+﻿using InnoTech.LegosForLife.Core.Models;
+using InnoTech.LegosForLife.Domain.IRepositories;
+using System;
+using System.Collections.Generic;
+
+namespace Innotech.LegosForLife.DataAccess.Repositories
+{
+    public class ProductRepository : IProductRepository
+    {
+        public List<Product> FindAll()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
