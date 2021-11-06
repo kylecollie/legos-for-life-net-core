9128a9d00eb50b4c5a0da10573a350fc38f9c5cf
Kyle Collie <kcollie@hotmail.com>
2021-11-06 10:30:33 -0400

    Add test for TindAll

--- InnoTech.LegosForLife.DataAccess.Test/ProductRepositoryTest.cs
+++ InnoTech.LegosForLife.DataAccess.Test/ProductRepositoryTest.cs
@@ -1,7 +1,12 @@
-﻿using EntityFrameworkCore.Testing.Moq;
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using EntityFrameworkCore.Testing.Moq;
+using InnoTech.LegosForLife.Core.Models;
+using Innotech.LegosForLife.DataAccess.Entities;
 using Innotech.LegosForLife.DataAccess.Repositories;
 using InnoTech.LegosForLife.Domain.IRepositories;
-using System.IO;
 using Xunit;
 
 namespace InnoTech.LegosForLife.DataAccess.Test
@@ -21,13 +26,59 @@ namespace InnoTech.LegosForLife.DataAccess.Test
         {
             Assert.Throws<InvalidDataException>(() => new ProductRepository(null));
         }
-        
+
         [Fact]
         public void ProductRepository_WithNullDbContext_ThrowsExceptionWithMessage()
         {
             var exception = Assert
                 .Throws<InvalidDataException>(() => new ProductRepository(null));
             Assert.Equal("Product Repository must have a DbContext", exception.Message);
+        }
+
+        [Fact]
+        public void FindAll_GetAllProductsEntitiesInDbContext_AsListOfProducts()
+        {
+            //Arrange
+            var fakeContext = Create.MockedDbContextFor<MainDbContext>();
+            var repository = new ProductRepository(fakeContext);
+            var list = new List<ProductEntity>
+            {
+                new ProductEntity{ Id = 1, Name = "Lego1"},
+                new ProductEntity{ Id = 2, Name = "Lego2"},
+                new ProductEntity{ Id = 3, Name = "Lego3"}
+            };
+            fakeContext.Set<ProductEntity>().AddRange(list);
+            fakeContext.SaveChanges();
+            var expectedList = list
+                .Select(pe => new Product
+                {
+                    Id = pe.Id,
+                    Name = pe.Name
+                })
+                .ToList();
+
+            //Act
+            var actualResult = repository.FindAll();
+
+            //Assert
+            Assert.Equal(expectedList, actualResult, new Comparer());
+        }
+    }
+
+    public class Comparer : IEqualityComparer<Product>
+    {
+        public bool Equals(Product x, Product y)
+        {
+            if (ReferenceEquals(x,y)) return true;
+            if (ReferenceEquals(x,null)) return false;
+            if (ReferenceEquals(y,null)) return false;
+            if (x.GetType() != y.GetType()) return false;
+            return x.Id == y.Id && x.Name == y.Name;
+        }
+
+        public int GetHashCode(Product obj)
+        {
+            return HashCode.Combine(obj.Id, obj.Name);
         }
     }
 }
--- Innotech.LegosForLife.DataAccess/Repositories/ProductRepository.cs
+++ Innotech.LegosForLife.DataAccess/Repositories/ProductRepository.cs
@@ -4,19 +4,25 @@ using InnoTech.LegosForLife.Domain.IRepositories;
 using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 
 namespace Innotech.LegosForLife.DataAccess.Repositories
 {
     public class ProductRepository : IProductRepository
     {
+        private readonly MainDbContext _ctx;
+
         public ProductRepository(MainDbContext ctx)
         {
             if (ctx == null) throw new InvalidDataException("Product Repository must have a DbContext");
+            _ctx = ctx;
         }
 
         public List<Product> FindAll()
         {
-            throw new NotImplementedException();
+            return _ctx.Products
+                .Select(pe => new Product { Id = pe.Id, Name = pe.Name })
+                .ToList();
         }
     }
 }
