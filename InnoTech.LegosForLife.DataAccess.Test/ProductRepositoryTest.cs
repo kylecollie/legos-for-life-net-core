d84cb2476e72030003471f69f68e48afdc808d29
Kyle Collie <kcollie@hotmail.com>
2021-11-06 08:04:51 -0400

    Add tests for null DbContext in repo constructor

--- InnoTech.LegosForLife.DataAccess.Test/ProductRepositoryTest.cs
+++ InnoTech.LegosForLife.DataAccess.Test/ProductRepositoryTest.cs
@@ -1,5 +1,7 @@
-﻿using Innotech.LegosForLife.DataAccess.Repositories;
+﻿using EntityFrameworkCore.Testing.Moq;
+using Innotech.LegosForLife.DataAccess.Repositories;
 using InnoTech.LegosForLife.Domain.IRepositories;
+using System.IO;
 using Xunit;
 
 namespace InnoTech.LegosForLife.DataAccess.Test
@@ -9,8 +11,23 @@ namespace InnoTech.LegosForLife.DataAccess.Test
         [Fact]
         public void ProductRepository_IsIProductRepository()
         {
-            var repository = new ProductRepository();
+            var fakeContext = Create.MockedDbContextFor<MainDbContext>();
+            var repository = new ProductRepository(fakeContext);
             Assert.IsAssignableFrom<IProductRepository>(repository);
+        }
+
+        [Fact]
+        public void ProductRepository_WithNullDbContext_ThrowsInvalidDataException()
+        {
+            Assert.Throws<InvalidDataException>(() => new ProductRepository(null));
+        }
+        
+        [Fact]
+        public void ProductRepository_WithNullDbContext_ThrowsExceptionWithMessage()
+        {
+            var exception = Assert
+                .Throws<InvalidDataException>(() => new ProductRepository(null));
+            Assert.Equal("Product Repository must have a DbContext", exception.Message);
         }
     }
 }
--- Innotech.LegosForLife.DataAccess/Repositories/ProductRepository.cs
+++ Innotech.LegosForLife.DataAccess/Repositories/ProductRepository.cs
@@ -1,12 +1,19 @@
 ﻿using InnoTech.LegosForLife.Core.Models;
+using InnoTech.LegosForLife.DataAccess;
 using InnoTech.LegosForLife.Domain.IRepositories;
 using System;
 using System.Collections.Generic;
+using System.IO;
 
 namespace Innotech.LegosForLife.DataAccess.Repositories
 {
     public class ProductRepository : IProductRepository
     {
+        public ProductRepository(MainDbContext ctx)
+        {
+            if (ctx == null) throw new InvalidDataException("Product Repository must have a DbContext");
+        }
+
         public List<Product> FindAll()
         {
             throw new NotImplementedException();
