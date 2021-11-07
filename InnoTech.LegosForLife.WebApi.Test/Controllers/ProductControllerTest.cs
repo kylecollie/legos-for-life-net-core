dcd90abad19fe2beec87611d7705ce208c92bcc1
Kyle Collie <kcollie@hotmail.com>
2021-11-07 09:19:13 -0500

    Add tests for service in controller

--- InnoTech.LegosForLife.WebApi.Test/Controllers/ProductControllerTest.cs
+++ InnoTech.LegosForLife.WebApi.Test/Controllers/ProductControllerTest.cs
@@ -1,20 +1,37 @@
-﻿using InnoTech.LegosForLife.Core.Models;
+﻿using InnoTech.LegosForLife.Core.IServices;
+using InnoTech.LegosForLife.Core.Models;
 using InnoTech.LegosForLife.WebApi.Controllers;
 using Microsoft.AspNetCore.Mvc;
+using Moq;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Reflection;
 using Xunit;
 
-namespace InnoTech.LegosForLife.WebApi.Test
+namespace InnoTech.LegosForLife.WebApi.Test.Controllers
 {
     public class ProductControllerTest
     {
         [Fact]
-        public void ProductController_IsOfTypeControllerBase()
+        public void ProductController_HasProductService_IsOfTypeControllerBase()
         {
-            var controller = new ProductController();
+            var service = new Mock<IProductService>();
+            var controller = new ProductController(service.Object);
             Assert.IsAssignableFrom<ControllerBase>(controller);
+        }
+
+        [Fact]
+        public void ProductController_WithNullProductService_ThrowsInvalidDataException()
+        {
+            Assert.Throws<InvalidDataException>(() => new ProductController(null));
+        }
+
+        [Fact]
+        public void ProductController_WithNullProductRepository_ThrowsExceptionWithMessage()
+        {
+            var exception = Assert.Throws<InvalidDataException>(() => new ProductController(null));
+            Assert.Equal("ProductService Cannot Be Null", exception.Message);
         }
 
         [Fact]
@@ -65,6 +82,17 @@ namespace InnoTech.LegosForLife.WebApi.Test
             var method = typeof(ProductController).GetMethods()
                 .FirstOrDefault(m => "GetAll".Equals(m.Name));
             Assert.Equal(typeof(ActionResult<List<Product>>).FullName, method.ReturnType.FullName);
+        }
+
+        [Fact]
+        public void GetAll_WithNoParams_HasGetHttpAttribute()
+        {
+            var methodInfo = typeof(ProductController)
+                .GetMethods()
+                .FirstOrDefault(m => m.Name == "GetAll");
+            var attr = methodInfo.CustomAttributes
+                .FirstOrDefault(ca => ca.AttributeType.Name == "HttpGetAttribute");
+            Assert.NotNull(attr);
         }
     }
 }
--- Innotech.LegosforLife.WebApi/Controllers/ProductController.cs
+++ Innotech.LegosforLife.WebApi/Controllers/ProductController.cs
@@ -1,6 +1,9 @@
-﻿using InnoTech.LegosForLife.Core.Models;
+﻿using InnoTech.LegosForLife.Core.IServices;
+using InnoTech.LegosForLife.Core.Models;
 using Microsoft.AspNetCore.Mvc;
+using System;
 using System.Collections.Generic;
+using System.IO;
 
 namespace InnoTech.LegosForLife.WebApi.Controllers
 {
@@ -8,6 +11,15 @@ namespace InnoTech.LegosForLife.WebApi.Controllers
     [ApiController]
     public class ProductController : ControllerBase
     {
+        public ProductController(IProductService productService)
+        {
+            if (productService is null)
+            {
+                throw new InvalidDataException("ProductService Cannot Be Null");
+            }
+        }
+
+        [HttpGet]
         public ActionResult<List<Product>> GetAll()
         {
             return null;
