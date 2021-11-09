eea23d44836d4a381b68bd3eb3b71419ff42cba7
Kyle Collie <kcollie@hotmail.com>
2021-11-09 08:03:48 -0500

    Continue config of DB

--- Innotech.LegosforLife.WebApi/Startup.cs
+++ Innotech.LegosforLife.WebApi/Startup.cs
@@ -44,13 +44,15 @@ namespace InnoTech.LegosForLife.WebApi
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, MainDbContext context)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
                 app.UseSwagger();
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Innotech.LegosforLife.WebApi v1"));
+                context.Database.EnsureCreated();
+                context.Database.EnsureCreated();
             }
 
             app.UseHttpsRedirection();
